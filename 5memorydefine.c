8da6dbe3bf01225deecd684e376a8771f28bac10
joy208das <158313870+joy208das@users.noreply.github.com>
2024-03-03 00:41:11 +0600

    Add files via upload

---
+++ 5memorydefine.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+  int age=25 ;
+  int *ptr= &age;
+  
+ printf("%u",ptr);
+ ptr++;
+ printf("\n");
+ printf("%u",ptr);
+
+    return 0;
+}
+// *ptr diye address store korlam
+// int ar memory size 4 bytes
+//tai ptr++ koray memory 4 bytes bere jabe
